e504e4abdf101ccf1637c5d330e0a28221493b0d
Kamil Gulzar <kamilgulzar289@gmail.com>
2024-01-16 16:42:54 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const n = document.getElementById("textarea")
+
+function num1(){
+    let number = 1;
+    n.value += number;
+}
+
+
+function num2(){
+    let number = 2;
+    n.value += number;
+}
+
+function num3(){
+    let number = 3;
+    n.value += number;
+}
+
+function add(){
+   let add = "+";
+   n.value += add;
+}
+
+function equal(){
+    
+}
+
